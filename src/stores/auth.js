bd31b874e27428d110587fad33f6b2fdb5697729
meryam <douirimeryam14@gmail.com>
2025-05-16 17:06:16 +0200

    Update auth.js
    user id toevoegen

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -10,13 +10,16 @@ export const useAuthStore = defineStore("auth", {
     setToken(token) {
       this.token = token;
       const payload = JSON.parse(atob(token.split(".")[1])); // Decode JWT payload
+      this.userId = payload.id  // <-- UserID opslaan
       this.expiresAt = payload.exp * 1000; // Convert to ms
 
       localStorage.setItem("auth_token", token);
+      localStorage.setItem("auth_userId", this.userId)
       localStorage.setItem("auth_expiresAt", this.expiresAt);
     },
     loadToken() {
       const token = localStorage.getItem("auth_token");
+      const userId = localStorage.getItem("auth_userId")
       const expiresAt = +localStorage.getItem("auth_expiresAt");
       if (token && Date.now() < expiresAt) {
         this.token = token;
