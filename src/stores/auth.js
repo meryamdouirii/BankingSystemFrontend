4176a747263109e06f73c24ac583946f26997f94
WesleyAmo <wesley2001.wa@gmail.com>
2025-05-16 15:49:41 +0200

    login working frontend

---
+++ src/stores/auth.js
@@ -0,0 +1,38 @@
+// stores/auth.js
+import { defineStore } from "pinia";
+
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    token: null,
+    expiresAt: null,
+  }),
+  actions: {
+    setToken(token) {
+      this.token = token;
+      const payload = JSON.parse(atob(token.split(".")[1])); // Decode JWT payload
+      this.expiresAt = payload.exp * 1000; // Convert to ms
+
+      localStorage.setItem("auth_token", token);
+      localStorage.setItem("auth_expiresAt", this.expiresAt);
+    },
+    loadToken() {
+      const token = localStorage.getItem("auth_token");
+      const expiresAt = +localStorage.getItem("auth_expiresAt");
+      if (token && Date.now() < expiresAt) {
+        this.token = token;
+        this.expiresAt = expiresAt;
+      } else {
+        this.logout();
+      }
+    },
+    logout() {
+      this.token = null;
+      this.expiresAt = null;
+      localStorage.removeItem("auth_token");
+      localStorage.removeItem("auth_expiresAt");
+    },
+    isAuthenticated() {
+      return this.token && Date.now() < this.expiresAt;
+    },
+  },
+});
